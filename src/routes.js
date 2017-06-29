8ed7c29ee29889390d19860fdf6cbc7fd6957e2a
linwr <54248422@qq.com>
2017-06-29 18:27:58 +0800

    edit

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+module.exports = [{
+	path: "/homepage",
+	name: "homepage",
+	component: function (resolve) {
+  	require(['./pages/homepage'], resolve);
+  },
+},
+{
+	path: "/page1",
+	name: "page1",
+	component: function (resolve) {
+  	require(['./pages/page1'], resolve);
+  },
+},
+{
+	path: "/page2",
+	name: "page2",
+	component: function (resolve) {
+  	require(['./pages/page2'], resolve);
+  },
+}];
