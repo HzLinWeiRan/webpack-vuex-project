3eac4cd0c8d2c9d79ac36fab07699fff50eb2f84
linweiran <linweiran@hzyushi.com>
2017-08-08 15:59:16 +0800

    init

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -7,7 +7,7 @@ const vuxLoader = require('vux-loader')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
 
-function resolve (dir) {
+function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
